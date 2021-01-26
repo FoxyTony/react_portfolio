2be6f7fe478e2a8f971d43eee34bf9bb5a300cae
FoxyTony <mirantony@icloud.com>
2021-01-26 22:59:18 +0100

    Failed on yarn start referring to unexpected error json 663

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM "react-dom";
+
+const HelloWorld = () => {
+    return <h2>Hello World</h2>
+};
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
